8732510207f011eb73be09a70cac2d7e218b2661
rian-gustandi <rian.gustandi17@gmail.com>
2020-07-08 09:26:40 +0700

    Add - New components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Component 
+import Album from './components/pages/album/album'
+
+// Layout 
+import Header from './components/pages/layout/header'
+import Jumbotron from './components/pages/layout/jombotron'
+import Footer from './components/pages/layout/footer'
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Header />
+        <main role="main">
+          <Jumbotron />
+          <Route exact path='/' component={Album} />
+        </main>
+        <Footer />
+      </BrowserRouter>
+    )
+  }
 }
-
-export default App;
